75c61c144f3b9dfef50efcde85ba9d3abe0a5b90
TioCaminhao <savio.v@escolar.ifrn.edu.br>
2024-07-23 23:36:38 -0300

    alterando o arquivo

---
+++ ClassCarro.py
@@ -0,0 +1,12 @@
+from ClassAutomovel import *
+
+class econômico(Automovel):
+  def dados(self):
+    self.litro = "10km por litro"
+    self.mtcar = "1.0 flex"
+
+class esportivo(Automovel):
+  def dados(self):
+    self.litro = '5km por litro'
+    self.mtcar = 'v8'
+
---
+++ saida.py
@@ -0,0 +1,10 @@
+from ClassAutomovel import *
+from ClassCarro import *
+
+
+ec1 = econômico("Honda", "Fit", "2008")
+ec1.dados()
+ec1.falardados()
+es1 = esportivo("Ford", "Maverick", "1979")
+es1.dados()
+es1.falardados()
